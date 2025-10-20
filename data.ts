996eb2d63f8d8481fe5a115c33c7a0ce55b6de24
sserranop2 <tucorreo@example.com>
2025-10-20 06:57:21 -0500

    taller 1 typescript

---
+++ data.ts
@@ -0,0 +1,58 @@
+import { Serie } from './Serie.js';
+
+export const series: Serie[] = [
+  new Serie(
+    1,
+    "Breaking Bad",
+    "AMC",
+    5,
+    "Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer.",
+    "https://www.amc.com/shows/breaking-bad",
+    "https://i.imgur.com/GGje0vc.jpg"
+  ),
+  new Serie(
+    2,
+    "Orange Is the New Black",
+    "Netflix",
+    6,
+    "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary.",
+    "https://www.netflix.com/co/title/70242311",
+    "https://i.imgur.com/EvKe48G.jpg"
+  ),
+  new Serie(
+    3,
+    "Game of Thrones",
+    "HBO",
+    7,
+    "American fantasy drama television series created by David Benioff and D. B. Weiss. It is an adaptation of A Song of Ice and Fire, George R. R. Martin's series of fantasy novels.",
+    "https://www.hbo.com/game-of-thrones",
+    "https://i.imgur.com/TDCEV1S.jpg"
+  ),
+  new Serie(
+    4,
+    "The Big Bang Theory",
+    "CBS",
+    12,
+    "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else. Enter beautiful, street-smart neighbor Penny.",
+    "https://www.cbs.com/shows/big_bang_theory/about/",
+    "https://i.imgur.com/uAEpVWk.jpg"
+  ),
+  new Serie(
+    5,
+    "Sherlock",
+    "BBC",
+    4,
+    "Sherlock depicts consulting detective Sherlock Holmes solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson.",
+    "https://www.bbc.co.uk/programmes/b018ttws",
+    "https://i.imgur.com/02B7qhj.jpg"
+  ),
+  new Serie(
+    6,
+    "A Very English Scandal",
+    "BBC",
+    2,
+    "A Very English Scandal is a fact-based three-part British television comedy-drama miniseries based on John Preston's book of the same name.",
+    "https://www.bbc.co.uk/programmes/p065smy4",
+    "https://i.imgur.com/D4y3DrQ.jpg"
+  )
+];
